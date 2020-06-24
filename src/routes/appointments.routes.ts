8f266bf3e2d40548f00dcf48af1ce6c2a3fb9b83
Jorge Dias <jorgedsdias@gmail.com>
2020-06-24 19:24:58 -0300

    Principios SOLID

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
@@ -14,26 +15,24 @@ appointmentsRouter.get('/', (req, res) => {
 });
 
 appointmentsRouter.post('/', (req, res) => {
-    const { provider, date } = req.body;
+    try {
+        const { provider, date } = req.body;
 
-    const parsedDate = startOfHour(parseISO(date));
+        const parsedDate = parseISO(date);
 
-    const findAppointmentInSameDate = appointmentRepository.findByDate(
-        parsedDate,
-    );
+        const createAppointmentService = new CreateAppointmentService(
+            appointmentRepository,
+        );
 
-    if (findAppointmentInSameDate) {
-        return res
-            .status(400)
-            .json({ message: 'This appointment is already booked' });
+        const appointment = createAppointmentService.execute({
+            provider,
+            date: parsedDate,
+        });
+
+        return res.json(appointment);
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
     }
-
-    const appointment = appointmentRepository.create({
-        provider,
-        date: parsedDate,
-    });
-
-    return res.json(appointment);
 });
 
 export default appointmentsRouter;
