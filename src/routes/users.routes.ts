759540551cd40e4ba5e492bf51b130289feba87e
Jorge <jorgedsdias@gmail.com>
2020-06-28 12:10:55 -0300

    Upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
     try {
         const { name, email, password } = req.body;
-
         const createUser = new CreateUserService();
-
         const user = await createUser.execute({
             name,
             email,
@@ -23,5 +27,27 @@ usersRouter.post('/', async (req, res) => {
         return res.status(400).json({ error: error.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (req, res) => {
+        try {
+            const updateUserAvatarService = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatarService.execute({
+                user_id: req.user.id,
+                avatarFilename: req.file.filename,
+            });
+
+            delete user.password;
+
+            return res.json(user);
+        } catch (error) {
+            return res.status(400).json({ error: error.message });
+        }
+    },
+);
 
 export default usersRouter;
