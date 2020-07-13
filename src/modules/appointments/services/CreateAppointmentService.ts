599e7ef0a14ebe8c90ff004aad4350d9145fa2ee
Jorge <jorgedsdias@gmail.com>
2020-07-13 20:54:16 -0300

    Refatoração principios SOLID

---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,47 @@
+import { startOfHour } from 'date-fns';
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppErro';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+interface IRequest {
+    provider_id: string;
+    date: Date;
+}
+
+// Liskov Substitution Principle
+// DIP (Dependency Inversion Principle)
+// SRP (Single Responsability Principle)
+
+@injectable()
+class CreateAppointmentService {
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository,
+    ) {}
+
+    public async execute({
+        date,
+        provider_id,
+    }: IRequest): Promise<Appointment> {
+        const appointmentDate = startOfHour(date);
+
+        const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (findAppointmentInSameDate) {
+            throw new AppError('This appointment is already booked');
+        }
+
+        const appointment = await this.appointmentsRepository.create({
+            provider_id,
+            date: appointmentDate,
+        });
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
--- src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
+++ src/modules/users/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
-import authConfig from '../config/auth';
-import AppError from '../errors/AppErro';
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/AppErro';
 
 interface TokenPayload {
     iat: number;
